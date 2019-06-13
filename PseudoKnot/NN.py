2672818ea6a707b9a11e1102817a090d7186fc80
Harshvardhan Singh <singhhar@oregonstate.edu>
2019-06-12 21:16:37 -0700

    Switched to 3 layer NN - Deep Learning

--- PseudoKnot/NN.py
+++ PseudoKnot/NN.py
@@ -9,110 +9,101 @@ import pdb
 import pandas as pd
 from sklearn.preprocessing import normalize
 
+class DatasetPseudoKnot (torch.utils.data.Dataset):
 
-train_dataset = np.genfromtxt("./feature103_Train.txt", delimiter="\t", dtype=None)
+    def __init__(self, file_path, transform = None):
+        trainColIdx = np.arange(1,len(train_dataset_raw[0]))
+        allData = np.loadtxt(sys.argv[4], dtype='float', delimiter = None, skiprows = 1, usecols = trainColIdx)
+        if (sys.argv[4].lower().find("train") != -1):
+            testData = allData[:,0]
+            x_norm = ((allData[:,1:] - allData[:,1:].min(0))) / (allData[:,1:].ptp(0)+1)
+            x_norm = np.column_stack((testData, x_norm))
+            self.data = torch.from_numpy(x_norm)
+            self.transform = None
+        else:
+            x_norm = (allData - allData.min(0)) / allData.ptp(0)
+            self.data = torch.from_numpy(x_norm)
+            self.transform = None
+
+    def __getitem__(self, index):
+        temp = []
+        temp_labels = []
+        if (self.data.shape[1] == num_features+1):  # 104 = with label
+            return self.data[index,1:].type(torch.FloatTensor), self.data[index][0].type(torch.LongTensor)
+        elif (self.data.shape[1] == num_features):  # 103 = without label
+            return self.data[index,0:].type(torch.FloatTensor)
+
+    def __len__(self):
+        return(len(self.data))
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(num_features, 200)
+        self.fc1_drop = nn.Dropout(float(sys.argv[1]))
+        self.fc2 = nn.Linear(200, 400)
+        self.fc2_drop = nn.Dropout(float(sys.argv[1]))
+        self.fc3 = nn.Linear(400, 100)
+        self.fc3_drop = nn.Dropout(float(sys.argv[1]))
+        self.fc4 = nn.Linear(100, 10)
+        self.fc4_drop = nn.Dropout(float(sys.argv[1]))
+        self.fc5 = nn.Linear(10, 2)
+
+    def forward(self, x):
+        x = x.view(-1, num_features)
+        x = F.leaky_relu(self.fc1(x), 0.2, True)
+        x = self.fc1_drop(x)
+        x = F.leaky_relu(self.fc2(x), 0.2, True)
+        x = self.fc2_drop(x)
+        x = F.leaky_relu(self.fc3(x), 0.2, True)
+        x = self.fc3_drop(x)
+        x = F.leaky_relu(self.fc4(x), 0.2, True)
+        x = self.fc4_drop(x)
+        return F.log_softmax(self.fc5(x),dim=1)
+
+
+
+data_set_raw = sys.argv[4]
+train_dataset_raw = np.genfromtxt(sys.argv[4], delimiter="\t", dtype=None)
 # inp = pd.DataFrame(train_dataset)
-pdb.set_trace()
+test_dataset_raw = None
+num_features = None
+
+if (sys.argv[4].lower().find("train") != -1):
+    num_features = len(train_dataset_raw[0])-2
+else:
+    num_features = len(train_dataset_raw[0])-1
 
 temp = []
 labels = []
 load_size = 100
 batch_size = load_size
-# temp = np.asarray(inp.iloc[0][1:])
-# for i in range(1,inp.shape[0]):
-#     # zz = np.append([x.iloc[1][1:]],zz,0)
-#     temp.append(inp.iloc[i][2:])       # MUCH faster (5 seconds vs 8-10 mins than the other commented methods)
-#     labels.append(inp.iloc[i][1])
-#     # temp = np.vstack((temp, inp.iloc[i][1:]))
-
-# # data = np.asarray(temp)
-# data = torch.FloatTensor(temp)
-# labels = torch.FloatTensor(labels)
 
 data_location = "./feature103_Train.txt"
 test_location = "./features103_test.txt"
+# 1053 features for allfeatures.txt
 
-train_size = train_dataset.shape[0]
+# train_size = 6000
+train_size = train_dataset_raw.shape[0]
 
-class DatasetPseudoKnot (torch.utils.data.Dataset):
+train_dataset = DatasetPseudoKnot(file_path=sys.argv[4])
 
-    def __init__(self, file_path, transform = None):
-        # train_dataset = np.genfromtxt(file_path, delimiter="\t", dtype=None)
-        trainColIdx = np.arange(1,105)
-        allData = np.loadtxt("feature103_Train.txt", dtype='float', delimiter = None, skiprows = 1, usecols = trainColIdx)
-        # trainData = allData[:,1:]
-        testData = allData[:,0]
-        x_norm = (allData[:,1:] - allData[:,1:].min(0)) / allData[:,1:].ptp(0)
-        x_norm = np.column_stack((testData, x_norm))
-        # pdb.set_trace()
-        # self.data = torch.from_numpy(np.random.rand(10000,104))
-        self.data = torch.from_numpy(x_norm)
-        # self.data = pd.DataFrame(train_dataset)
-        self.transform = None
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=load_size, shuffle=False)
 
-    def __getitem__(self, index):
-        temp = []
-        temp_labels = []
-        # for i in range(1,self.data.shape[0]):
-        #     # zz = np.append([x.iloc[1][1:]],zz,0)x
-        #     temp.append(self.data.iloc[i][2:])       # MUCH faster (5 seconds vs 8-10 mins than the other commented methods)
-        #     temp_labels.append(self.data.iloc[i][1])
-        # rows = None
-        # label = None
-        # pdb.set_trace()
-        return self.data[index,1:].type(torch.FloatTensor), self.data[index][0].type(torch.LongTensor)
+test_dataset = DatasetPseudoKnot(file_path=sys.argv[4])
+test_loader = torch.utils.data.DataLoader(train_dataset, shuffle=False)
 
-        # if (self.data.iloc[index].size != 104):
-        #     # df_norm = (df - df.mean()) / (df.max() - df.min())
-        #     rows = self.data.iloc[index][2:]
-        #     label = torch.LongTensor(self.data.iloc[index][1:])
-        #     norm_rows = torch.FloatTensor((rows - rows.mean())/rows.max() - rows.min())
-        #     if self.transform is not None:
-        #         norm_rows = self.transform(norm_rows)
-        #     return norm_rows, label[0]
-        # else:
-        #     rows = self.data.iloc[index][1:]
-        #     norm_rows = torch.FloatTensor((rows - rows.mean())/rows.max() - rows.min())
-        #     return norm_rows
-            # label = torch.LongTensor(self.data.iloc[index][1:])
-        # print(label)
-        # print(self.data.iloc[index][0])
-        
+if (len(sys.argv) == 6):
+    test_dataset = DatasetPseudoKnot(file_path=sys.argv[5])
+    test_loader = torch.utils.data.DataLoader(test_dataset, shuffle=False)
+    test_dataset_raw = np.genfromtxt(sys.argv[5], delimiter="\t", dtype=None)
 
-    def __len__(self):
-        return(len(self.data))
-
-train_dataset = DatasetPseudoKnot(file_path=data_location)
-
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=load_size, shuffle=True)
-
-test_dataset = DatasetPseudoKnot(file_path=test_location)
-
-test_loader = torch.utils.data.DataLoader(test_dataset, shuffle=False)
 # img, lab = train_dataset.__getitem__(0)
 
-num_epochs = 20
+num_epochs = 50
 train_errs = torch.zeros(num_epochs)
 val_errs = torch.zeros(num_epochs)
 
-# pdb.set_trace()
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(103, 200)
-        self.fc1_drop = nn.Dropout(float(sys.argv[1]))
-        self.fc2 = nn.Linear(200, 2)
-        # self.fc2_drop = nn.Dropout(float(sys.argv[1]))
-        # self.fc3 = nn.Linear(100, 2)
-
-    def forward(self, x):
-        x = x.view(-1,103)
-        x = F.leaky_relu(self.fc1(x), 0.2, True)
-        x = self.fc1_drop(x)
-        # x = F.leaky_relu(self.fc2(x), 0.2, True)
-        # x = self.fc2_drop(x)
-        return F.log_softmax(self.fc2(x),dim=1)
 
 #identify device for pytorch, use gpu if available
 if torch.cuda.is_available():
@@ -127,6 +118,8 @@ def train(epoch, log_interval,optimizer, criterion,):
     global model
     global val_errs
     global train_errs
+    global num_iterations_without_change    
+    global epochs_label_results_last_four
     #set model to training mode
     model.train()
     val_correct = 0
@@ -146,7 +139,6 @@ def train(epoch, log_interval,optimizer, criterion,):
             
             # Pass data through the network
             output = model(data)    
-            # pdb.set_trace()
             # print(batch_idx)
             # Calculate loss
             criterion = nn.CrossEntropyLoss()
@@ -169,8 +161,8 @@ def train(epoch, log_interval,optimizer, criterion,):
             no_knot += torch.nonzero(pred == 0).shape[0]
             print("Has_knot is", has_knot, no_knot)            
             val_correct += pred.eq(target.data).sum()
-    print(val_correct)
-    print(inside)
+    # print(val_correct)
+    # print(inside)
     # pdb.set_trace()
     # val_errs[epoch] = ((27348 - train_size) - val_correct.item())/ (27348 - train_size)
     # val_errs[epoch] = (7358 - val_correct.item())/ 7358
@@ -186,12 +178,34 @@ def train(epoch, log_interval,optimizer, criterion,):
             train_correct += pred.eq(target.data).sum()
             has_knot += torch.nonzero(pred == 1).shape[0]
             no_knot += torch.nonzero(pred == 0).shape[0]
-            print("Has_knot is", has_knot, no_knot)            
+            print("Has_knot is", has_knot, no_knot)
             # pdb.set_trace()
     print("train_correct is ", train_correct)
     train_errs[epoch] = (train_size-train_correct.item())/(train_size)
     print("training error for epoch "+str(epoch+1)+": "+str(train_errs[epoch].item()))
-    print("validation error for epoch " +str(epoch+1)+": "+str(val_errs[epoch].item()))
+    # print("validation error for epoch " +str(epoch+1)+": "+str(val_errs[epoch].item()))
+    epochs_label_results_last_four.append(has_knot)
+    if (len(epochs_label_results_last_four) == 3):
+        average = (epochs_label_results_last_four[0] + epochs_label_results_last_four[1] + epochs_label_results_last_four[2])/3
+        if (average - has_knot > 600):
+            model = Net().to(device)
+            print("model not generalizing well, restarting")        
+        epochs_label_results_last_four = []
+    if (epoch == num_iterations_without_change and no_knot == 0):
+        num_iterations_without_change += 2
+        model = Net().to(device)
+        print("model not generalizing well, restarting")        
+    if (has_knot < 2500):
+        if (epoch == 1 or has_knot == 0):
+            model = Net().to(device)
+            return False
+        return True
+    if (epoch >= num_iterations_without_change and no_knot < 200):
+        num_iterations_without_change = epoch + 2
+        model = Net().to(device)
+
+epochs_label_results_last_four = []
+num_iterations_without_change = 3
     # pdb.set_trace()
 
 def test():
@@ -200,17 +214,19 @@ def test():
     has_knot = 0
     no_knot = 0
     for batch_idx, (data, target) in enumerate(test_loader):
+        # pdb.set_trace()
         output = model(data)
         pred = output.data.max(1)[1]
         test_correct += pred.eq(target.data).sum()
         # pdb.set_trace()
         has_knot += torch.nonzero(pred == 1).shape[0]
         no_knot += torch.nonzero(pred == 0).shape[0]
-    print("Entire testing set accuracy is ", test_correct.type(torch.FloatTensor)/27348)
+    print("Entire testing set accuracy is ", test_correct.type(torch.FloatTensor)/test_dataset.__len__())
     # pdb.set_trace()
 
 def actual_test():
     global model
+    f = open("results.txt", "w")
     test_correct = 0
     has_knot = 0
     no_knot = 0
@@ -220,8 +236,13 @@ def actual_test():
         pred = output.data.max(1)[1]
         # test_correct += pred.eq(target.data).sum()
         # pdb.set_trace()
-        has_knot += torch.nonzero(pred == 1).shape[0]
-        no_knot += torch.nonzero(pred == 0).shape[0]
+        has_knot = torch.nonzero(pred == 1).shape[0]
+        no_knot = torch.nonzero(pred == 0).shape[0]
+        if (has_knot == 1):
+            f.write(str(test_dataset_raw[batch_idx][0]).replace("\'", "") + ",1")
+        else:
+            f.write(str(test_dataset_raw[batch_idx][0]).replace("\'", "") + ",0")
+
     pdb.set_trace()
     print("test accuracy")
     # print(test_correct)
@@ -230,26 +251,37 @@ def actual_test():
 def main():
     global device
     #check for valid number of arguments
-    if(len(sys.argv) != 4):
+    if(len(sys.argv) <= 4):
         print("please enter dropout, momentum, and weight decay in command line")
         return
     #check for valid float as learning rate
     #read in learning rate from command line
-    d = float(sys.argv[1])
-    m = float(sys.argv[2])
-    wd = float(sys.argv[3])
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.000007, momentum=m, weight_decay=wd)
+    d = float(sys.argv[1])      # dropout
+    m = float(sys.argv[2])      # momentum
+    wd = float(sys.argv[3])     # wieght-decay
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0000032558, momentum=m, weight_decay=wd)
     criterion = nn.CrossEntropyLoss()
     print(model)
+    stopped = 0
     for i in range(num_epochs):
-        train(i,200,optimizer,criterion)
+        stop = train(i,200,optimizer,criterion)
+        if (stop == True): 
+            break
+            stopped = i
+        stopped = i
+
     print("training errors")
     #plot results
-    test()
-    xvals = torch.arange(1,num_epochs+1)
-    trainingLine = plt.plot(xvals.numpy(),train_errs.numpy(), label='Training Error')
-    valLine = plt.plot(xvals.numpy(),val_errs.numpy(), label='Validation Error')
-    plt.title("Error vs Epochs for momentum="+str(m)+" using ReLu")
+    if (len(sys.argv) == 6):
+        actual_test()
+    else:
+        test()
+    xvals = torch.arange(1,stopped+2)
+    pdb.set_trace()
+    trainingLine = plt.plot(xvals.numpy(),train_errs.numpy()[:stopped+1], label='Training Error')
+    # valLine = plt.plot(xvals.numpy(),val_errs.numpy(), label='Validation Error')
+    # pdb.set_trace()
+    plt.title("Error vs Epochs for momentum="+str(m)+" using leaky_ReLu for TRAINING")
     plt.ylabel("Error Percentage")
     plt.xlabel("Epochs")
     plt.legend(loc='best')
